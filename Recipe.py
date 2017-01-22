9d09aef1deca3ddc178118fa02bde2f8785f2433
David Kelly <dk6343@mcla.edu>
2017-01-22 17:31:00 +0000

    Adding Recipe Converter

--- Recipe.py
+++ Recipe.py
@@ -8,10 +8,24 @@ print "Flour:",
 flour = raw_input()
 print "Water:",
 water = raw_input()
-print "Salt",
+print "Salt:",
 salt = raw_input()
-print "Yeast",
+print "Yeast:",
 yeast = raw_input()
+print "Enter loaf adjustment factor (e.g. 2.0 for double the size):",
+factor = raw_input()
 
 print "--Modified Recipe--"
-print "Flour %.2f cups" % int(flour)*float(2.0)
+
+# Hi David, notice that I added () around your calculation
+print "Flour %.2f cups." % (int(flour)*int(factor))
+print "Water %.2f cups." % (int (water)*int(factor))
+print "Salt %.2f teaspoons." % (int(salt)*int(factor))
+print "Yeast %.2f teaspoons" % (int(yeast)*int(factor))
+
+print "--Modified Recipe in grams--"
+
+print "Flour %.2f g." % (int(flour)*int(factor)*125.0)
+print "Water %.2f g." % (int(water)*int(factor)*236.0)
+print "Salt %.2f g." % (int(salt)*int(factor)*7.0)
+print "Yeast %.2f g." % (int(yeast)*int(factor)*3.0)
